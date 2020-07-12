968840f4ad1827425e32a401981afe1785d4841a
Eric Skala <e.skala@reply.de>
2020-07-12 22:42:33 +0200

    CHG: Use brew bundle

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 brew_setup () {
 	which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && \
-	brew tap homebrew/cask-versions && brew tap homebrew/cask-fonts && \
-	cat Brewfile | sed 's/#.*$$//g' | grep -v '^$$' | xargs -n1 brew install
-}
-
-cask_setup () {
-	cat Caskfile | xargs -n1 brew cask install
+	brew bundle
 }
 
 config_setup () {
@@ -16,17 +11,11 @@ link_setup () {
 	find $(PWD) -name ".*" -type f -exec ln -sF {} ~/ ';'
 }
 
-mas_install () {
-	which mas && bash ./masfile
-}
-
 oh-my-zsh () {
 	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 }
 
 brew_setup
-cask_setup
 config_setup
 link_setup
-mas_install
 oh-my-zsh
